764bebccecfe4e0f74cf773efcb7ba4bf3eea616
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-07-31 12:43:10 +0200

    implements reflection intetgration test

--- TicketDispenser.md
+++ TicketDispenser.md
@@ -27,7 +27,9 @@
         - the `protected` method will remain uncovered for this test
 
 ### Notes
-- Fix the initial unit test
-    - This test is very fragile, because in case that there are tests that are executed before it, they would alter the final result incrementing the `turnNumber` making the result not constant
+- Integration
+    - Fix the initial integration test
+        - This test is very fragile, because in case that there are tests that are executed before it, they would alter the final result incrementing the `turnNumber` making the result not constant
+    - Implements the `reflection` integration test
 
 ### Conclusions
--- tests/TurnTicketDispenser/TicketDispenserTest.php
+++ tests/TurnTicketDispenser/TicketDispenserTest.php
@@ -6,6 +6,8 @@ namespace Tests\TurnTicketDispenser;
 
 use PHPUnit\Framework\TestCase;
 use RacingCar\TurnTicketDispenser\TicketDispenser;
+use RacingCar\TurnTicketDispenser\TurnNumberSequence;
+use ReflectionClass;
 
 class TicketDispenserTest extends TestCase
 {
@@ -14,5 +16,20 @@ class TicketDispenserTest extends TestCase
         $dispenser = new TicketDispenser();
         $ticket = $dispenser->getTurnTicket();
         $this->assertSame(0, $ticket->getTurnNumber());
+    }
+
+    public function testReflectionShouldReturnZeroAsTurnNumber(): void
+    {
+        $reflectedTurnNumberSequence = new ReflectionClass(TurnNumberSequence::class);
+        $reflectedfTurnNumber = $reflectedTurnNumberSequence->getProperty('turnNumber');
+        $oldTurnNumberValue = $reflectedfTurnNumber->getValue();
+        $reflectedfTurnNumber->setValue(45);
+
+        $dispenser = new TicketDispenser();
+        $ticket = $dispenser->getTurnTicket();
+
+        $this->assertSame(45, $ticket->getTurnNumber());
+
+        $reflectedfTurnNumber->setValue($oldTurnNumberValue);
     }
 }
